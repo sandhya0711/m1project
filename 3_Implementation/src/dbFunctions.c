644d7efa39b8fec6a8ba49aaf397ceb34096405e
sandhya0711 <101318030+sandhya0711@users.noreply.github.com>
2022-04-03 12:26:49 +0530

    Update dbFunctions.c

--- 3_Implementation/src/dbFunctions.c
+++ 3_Implementation/src/dbFunctions.c
@@ -1,9 +1,9 @@
 /**
  * @file dbFunctions.c
- * @author Animesh Srivastava (animeshs1798@gmail.com)
+ * @author sandhya
  * @brief 
  * @version 0.1
- * @date 2021-04-13
+ * 
  * 
  * @copyright Copyright (c) 2021
  * 
